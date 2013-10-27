838f24a72df803086034bdf4be2e63e3dc7519e5
ferron <ferronrsmith@gmail.com>
2013-10-27 03:11:04 -0400

    update lib

--- js/directives/directives.js
+++ js/directives/directives.js
@@ -1,3 +1,4 @@
+/*jslint unparam : true*/
 /*global todomvc, angular  */
 
 
@@ -22,11 +23,11 @@ todomvc.directive('ngEvent', ['$parse',
                     var params = Array.prototype.slice.call(arguments);
                     //Take out first parameter (event object);
                     params = params.splice(1);
-					fn(scope, {$event: evt, $params: params});
-					// check if if there is something to digest
-					if (!scope.$$phase) {
-						scope.$apply();
-					}
+                    fn(scope, {$event: evt, $params: params});
+                    // check if if there is something to digest
+                    if (!scope.$$phase) {
+                        scope.$apply();
+                    }
                 });
             });
         };
@@ -55,7 +56,7 @@ todomvc.directive('ngValidate', function () {
         restrict: 'A',
         require: 'ngModel',
         link: function (scope, elm, attrs, ctrl) {
-            var validateFn, watch, validators = {},
+            var validateFn, validators = {},
                 validateExpr = scope.$eval(attrs.ngValidate);
 
             if (!validateExpr) {
@@ -66,39 +67,81 @@ todomvc.directive('ngValidate', function () {
                 validateExpr = { validator: validateExpr };
             }
 
-            angular.forEach(validateExpr, function (expression, key) {
+            angular.forEach(validateExpr, function (exprssn, key) {
                 validateFn = function (valueToValidate) {
-                    var result;
-                    if (scope.$eval(expression, { '$value' : valueToValidate })) {
+                    var expression = scope.$eval(exprssn, { '$value': valueToValidate });
+                    if (angular.isObject(expression) && angular.isFunction(expression.then)) {
+                        // expression is a promise
+                        expression.then(function () {
+                            ctrl.$setValidity(key, true);
+                        }, function () {
+                            ctrl.$setValidity(key, false);
+                        });
+//                        return valueToValidate;
+                    } else if (expression) {
+                        // expression is true
                         ctrl.$setValidity(key, true);
-                        result = valueToValidate;
+//                        return valueToValidate;
                     } else {
+                        // expression is false
                         ctrl.$setValidity(key, false);
-                        result = undefined;
+//                        return undefined;
                     }
-                    return result;
+                    return valueToValidate;
                 };
                 validators[key] = validateFn;
                 ctrl.$formatters.push(validateFn);
                 ctrl.$parsers.push(validateFn);
             });
 
-            // Support for ng-validate-watch
-            if (attrs.ngValidateWatch) {
-                watch = scope.$eval(attrs.ngValidateWatch);
+            function apply_watch(watch) {
+                //string - update all validators on expression change
                 if (angular.isString(watch)) {
                     scope.$watch(watch, function () {
-                        angular.forEach(validators, function (validatorFn, key) {
+                        angular.forEach(validators, function (validatorFn) {
                             validatorFn(ctrl.$modelValue);
                         });
                     });
-                } else {
-                    angular.forEach(watch, function (expression, key) {
+                    return;
+                }
+
+                //array - update all validators on change of any expression
+                if (angular.isArray(watch)) {
+                    angular.forEach(watch, function (expression) {
                         scope.$watch(expression, function () {
-                            validators[key](ctrl.$modelValue);
+                            angular.forEach(validators, function (validatorFn) {
+                                validatorFn(ctrl.$modelValue);
+                            });
                         });
                     });
+                    return;
                 }
+
+                //object - update appropriate validator
+                if (angular.isObject(watch)) {
+                    angular.forEach(watch, function (expression, validatorKey) {
+                        //value is string - look after one expression
+                        if (angular.isString(expression)) {
+                            scope.$watch(expression, function () {
+                                validators[validatorKey](ctrl.$modelValue);
+                            });
+                        }
+
+                        //value is array - look after all expressions in array
+                        if (angular.isArray(expression)) {
+                            angular.forEach(expression, function (intExpression) {
+                                scope.$watch(intExpression, function () {
+                                    validators[validatorKey](ctrl.$modelValue);
+                                });
+                            });
+                        }
+                    });
+                }
+            }
+
+            // Support for ng-validate-watch
+            if (attrs.ngValidateWatch) {
+                apply_watch(scope.$eval(attrs.ngValidateWatch));
             }
         }
     };
